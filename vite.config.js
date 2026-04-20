72b2574b02609ce2d63f29614570888b01bedaeb
lagilogi <wouter_s@hotmail.nl>
2026-04-20 16:12:25 +0200

    VITE CONFIG: Add build property to config to make multi-page possible. TSCONFIG.JSON: Add file to define compiler options and project boundaries for TypeScript.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/portfolio-website/'
+  base: '/portfolio-website/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        reaction: resolve(__dirname, 'reaction-time/index.html'),
+      },
+    },
+  },
 })

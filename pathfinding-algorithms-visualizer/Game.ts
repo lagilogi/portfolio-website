4dac512ba0e1b2c3de88ca57343edb568261e13a
lagilogi <wouter_s@hotmail.nl>
2026-05-04 19:12:09 +0200

    Add more class members for event listeners and their values, and loop variables, modify constructor for the added class members, add event 4 of 5 event listener handlers, add reset, getPath, and loop functions

--- pathfinding-algorithms-visualizer/Game.ts
+++ pathfinding-algorithms-visualizer/Game.ts
@@ -1,44 +1,123 @@
-import { Cell, CellType, gameState } from './types'
+import { Cell, CellType, GameState, PathfindingAlgorithm, Maze } from './types'
 import mazes from './mazes'
 import { Renderer } from './Renderer';
+import { BFS } from './bfs';
 
 export class Game {
-  renderer: Renderer;
-  mazeSelect: HTMLElement
-  state: gameState;
+  startButton: HTMLElement;
+  resetButton: HTMLElement;
+  mazeSelect: HTMLElement;
+  algorithmSelect: HTMLElement;
+  speedSlider: HTMLElement;
 
+  algorithm: PathfindingAlgorithm;
+  renderer: Renderer;
+  state: GameState;
+  stepTime: number;
+  timePassed: number;
+
+  currMaze: Maze;
   grid!: Cell[][];
+  path: Cell[];
 
   constructor() {
-    this.renderer = new Renderer();
+    this.startButton = document.getElementById('startButton')!
+    this.resetButton = document.getElementById('resetButton')!
     this.mazeSelect = document.getElementById('mazeSelect')!
-    this.state = gameState.READY;
+    this.algorithmSelect = document.getElementById('algorithmSelect')!
+    this.speedSlider = document.getElementById('speedSlider')!
 
-    this.createGrid(mazes[0].maze)
+
+    this.renderer = new Renderer();
+    this.state = GameState.READY;
+    this.stepTime = 1000000; // 1s
+    this.timePassed = 0;
+    this.path = []
+
+    this.currMaze = mazes[0]
+    this.createGrid(this.currMaze.maze)
+    this.algorithm = new BFS(this.grid, this.currMaze);
+
     this.bindEvents()
   }
 
-
-
   bindEvents() {
+    this.startButton.addEventListener("click", this.handleStartButton);
+    this.resetButton.addEventListener("click", this.handleResetButton);
     this.mazeSelect.addEventListener("change", this.handleMazeChange);
+    this.algorithmSelect.addEventListener("change", this.handleAlgorithmChange);
+    // this.speedSlider.addEventListener("change", this.handleSpeedSliderChange);
   }
 
 
-  getMazeIndex(mazeName: string): number {
-    for (let i = 0; i < mazes.length; i++) {
-      if (mazes[i].name === mazeName)
-        return i
+  // ---- EVENT HANDLERS ----
+  // Starts or pauses the visualizer loop
+  handleStartButton = () => {
+    if (this.algorithm === null)
+      return
+    if (this.state === GameState.FINISHED) {
+      this.state = GameState.RESETTING
+      this.reset()
     }
-    return 0
+    if (this.state === GameState.READY) {
+      this.state = GameState.RUNNING
+      // this.loop(0)
+    }
+    else if (this.state === GameState.PAUSED)
+      this.state = GameState.RUNNING
+    else if (this.state === GameState.RUNNING)
+      this.state = GameState.PAUSED
   }
 
+  // Resets the game when the reset button is clicked
+  handleResetButton = () => {
+    this.state = GameState.RESETTING
+    this.reset()
+  }
+
+  // This function gets called when another maze is selected from the dropdown menu
   handleMazeChange = (event: Event) => {
-    const select = event.target as HTMLSelectElement
+    this.state = GameState.RESETTING
 
-    this.createGrid(mazes[this.getMazeIndex(select.value)].maze)
+    const select = event.target as HTMLSelectElement
+    const mazeName: string = select.value
+    let index: number
+
+    for (index = 0; index < mazes.length; index++) {
+      if (mazes[index].name === mazeName)
+        break;
+    }
+    this.currMaze = index < mazes.length ? mazes[index] : mazes[3]
+    this.createGrid(this.currMaze.maze)
+    this.reset()
   }
 
+  handleAlgorithmChange = (event: Event) => {
+    this.state = GameState.RESETTING
+
+    const select = event.target as HTMLSelectElement
+    const algorithmName: string = select.value
+
+    switch (algorithmName) {
+      case 'BFS':
+        this.algorithm = new BFS(this.grid, this.currMaze)
+        break;
+    
+      default:
+        this.algorithm = new BFS(this.grid, this.currMaze)
+        break;
+    }
+
+    this.reset()
+  }
+
+  reset() {
+    this.timePassed = 0
+    this.path = []
+    this.algorithm.reset(this.grid, this.currMaze)
+    this.renderer.renderGrid(this.grid)
+    this.state = GameState.READY
+  }
 
 
   // Creates a double array containing an object per cell with information
@@ -78,8 +157,9 @@ export class Game {
     }
 
     return {
-      row,
-      col,
+      id: `${row}-${col}`,
+      row: row,
+      col: col,
       type: type,
       visited: false,
       parent: null,
@@ -87,8 +167,38 @@ export class Game {
     }
   }
 
-  bfs() {
-    
+  // Retrieves the path, backwards, from end to finish, by getting the parent of the cells. The parents of cells are determined by the algorithm
+  getPath(endCell: Cell) {
+    let currCell: Cell = endCell.parent!
+
+    while (currCell.type !== CellType.START) {
+      this.path.push(currCell)
+      currCell = currCell.parent!
+    }
+  }
+
+  loop(interval: number) {
+    this.timePassed += interval
+    if (this.state === GameState.RUNNING) {
+      if (this.timePassed > this.stepTime) {
+        this.timePassed = 0
+        const result: Cell | null = this.algorithm.step();
+  
+        if (result === null)
+          return
+        else if (result.type === CellType.END) {
+          this.getPath(result)
+          this.renderer.renderPath(this.path)
+          this.state = GameState.FINISHED
+        }
+        else
+          this.renderer.renderVisitedCell(result)
+      }
+    }
+    if (this.state !== GameState.RUNNING && this.state !== GameState.PAUSED)
+      return
+
+    requestAnimationFrame(this.loop)
   }
 
 

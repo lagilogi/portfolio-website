1c55751df480b63b44a38d681e7ec0e0689d9e07
lagilogi <wouter_s@hotmail.nl>
2026-05-04 19:06:19 +0200

    Add more GameStates, add PathfindingAlgorithm interface, add Maze interface

--- pathfinding-algorithms-visualizer/types.ts
+++ pathfinding-algorithms-visualizer/types.ts
@@ -1,7 +1,9 @@
-export enum gameState {
+export enum GameState {
   READY,
   RUNNING,
-  FINISHED
+  PAUSED,
+  FINISHED,
+  RESETTING
 }
 
 export enum CellType {
@@ -12,10 +14,25 @@ export enum CellType {
 }
 
 export interface Cell {
+  id: string
   row: number,
   col: number,
   type: CellType,
   visited: boolean,
   parent: Cell | null,
   weight: number | null,
+}
+
+export interface PathfindingAlgorithm {
+  start: Cell,
+  step(): Cell | null,
+  getCell(cellCoords: number[]): Cell,
+  reset(grid: Cell[][], currMaze: Maze): void,
+}
+
+export interface Maze {
+  name: string,
+  start: number[],
+  end: number[],
+  maze: number[][]
 }

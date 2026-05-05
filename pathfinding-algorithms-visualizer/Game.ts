5bb2bb6a5f54fc7fb604b47da4e2d1ef837f2e6b
lagilogi <wouter_s@hotmail.nl>
2026-05-05 18:44:03 +0200

    Fix loop function, fix getPath function, modify createGrid function to return a grid double array, add class members and declaration in constructor, implement handleSpeedSliderChange function, add and modify imports

--- pathfinding-algorithms-visualizer/Game.ts
+++ pathfinding-algorithms-visualizer/Game.ts
@@ -1,7 +1,8 @@
-import { Cell, CellType, GameState, PathfindingAlgorithm, Maze } from './types'
+import { Cell, CellType, CellState, GameState, PathfindingAlgorithm, Maze } from './types'
 import mazes from './mazes'
 import { Renderer } from './Renderer';
-import { BFS } from './bfs';
+import { BFS } from './Bfs';
+import { DFS } from './Dfs';
 
 export class Game {
   startButton: HTMLElement;
@@ -10,15 +11,16 @@ export class Game {
   algorithmSelect: HTMLElement;
   speedSlider: HTMLElement;
 
+  currMaze: Maze;
+  grid!: Cell[][];
+  path: Cell[];
+
   algorithm: PathfindingAlgorithm;
   renderer: Renderer;
   state: GameState;
   stepTime: number;
   timePassed: number;
-
-  currMaze: Maze;
-  grid!: Cell[][];
-  path: Cell[];
+  lastTimestamp: number;
 
   constructor() {
     this.startButton = document.getElementById('startButton')!
@@ -27,15 +29,15 @@ export class Game {
     this.algorithmSelect = document.getElementById('algorithmSelect')!
     this.speedSlider = document.getElementById('speedSlider')!
 
-
     this.renderer = new Renderer();
     this.state = GameState.READY;
-    this.stepTime = 1000000; // 1s
-    this.timePassed = 0;
-    this.path = []
+    this.stepTime = 100;
+    this.timePassed = this.stepTime + 1;
+    this.lastTimestamp = 0;
 
     this.currMaze = mazes[0]
-    this.createGrid(this.currMaze.maze)
+    this.grid = this.createGrid(this.currMaze.maze)
+    this.path = []
     this.algorithm = new BFS(this.grid, this.currMaze);
 
     this.bindEvents()
@@ -46,22 +48,21 @@ export class Game {
     this.resetButton.addEventListener("click", this.handleResetButton);
     this.mazeSelect.addEventListener("change", this.handleMazeChange);
     this.algorithmSelect.addEventListener("change", this.handleAlgorithmChange);
-    // this.speedSlider.addEventListener("change", this.handleSpeedSliderChange);
+    this.speedSlider.addEventListener("change", this.handleSpeedSliderChange);
   }
 
 
   // ---- EVENT HANDLERS ----
   // Starts or pauses the visualizer loop
   handleStartButton = () => {
-    if (this.algorithm === null)
-      return
     if (this.state === GameState.FINISHED) {
       this.state = GameState.RESETTING
       this.reset()
     }
     if (this.state === GameState.READY) {
       this.state = GameState.RUNNING
-      // this.loop(0)
+      this.lastTimestamp = performance.now()
+      requestAnimationFrame(this.loop)
     }
     else if (this.state === GameState.PAUSED)
       this.state = GameState.RUNNING
@@ -102,6 +103,9 @@ export class Game {
       case 'BFS':
         this.algorithm = new BFS(this.grid, this.currMaze)
         break;
+      case 'DFS':
+        this.algorithm = new DFS(this.grid, this.currMaze)
+        break;
     
       default:
         this.algorithm = new BFS(this.grid, this.currMaze)
@@ -111,18 +115,44 @@ export class Game {
     this.reset()
   }
 
+  handleSpeedSliderChange = (event: Event) => {
+    const slider = event.target as HTMLInputElement
+
+    console.log(slider.value)
+
+    switch (slider.value) {
+      case '1':
+        this.stepTime = 500;
+        break;
+      case '2':
+        this.stepTime = 300;
+        break;
+      case '3':
+        this.stepTime = 150;
+        break;
+      case '4':
+        this.stepTime = 50;
+        break;
+      case '5':
+        this.stepTime = 0;
+        break;
+    }
+  }
+
   reset() {
-    this.timePassed = 0
-    this.path = []
-    this.algorithm.reset(this.grid, this.currMaze)
-    this.renderer.renderGrid(this.grid)
-    this.state = GameState.READY
+    this.grid = this.createGrid(this.currMaze.maze);
+    this.timePassed = this.stepTime + 1;
+    this.path = [];
+    this.algorithm.reset(this.grid, this.currMaze);
+    this.renderer.renderGrid(this.grid);
+    this.state = GameState.READY;
+    console.log('reset inside Game', this.grid)
   }
 
 
   // Creates a double array containing an object per cell with information
-  createGrid(maze: number[][]) {
-    console.log(maze)
+  createGrid(maze: number[][]): Cell[][] {
+    this.grid = []
     const rows: number = maze.length
     const cols: number = maze[0].length
     const newGrid: Cell[][] = []
@@ -135,24 +165,29 @@ export class Game {
       newGrid.push(newRow)
     }
     this.renderer.renderGrid(newGrid);
-    this.grid = newGrid
+    return newGrid;
   }
 
   // Creates individual cells to be added to the grid double array
   createCell(row: number, col: number, type: number): Cell {
-    let cellType: CellType
+    let cellType: CellType;
+    let cellState: CellState
     switch (type) {
       case CellType.FLOOR:
-        cellType = CellType.FLOOR
+        cellType = CellType.FLOOR;
+        cellState = CellState.OPEN;
         break;
       case CellType.WALL:
-        cellType = CellType.FLOOR
+        cellType = CellType.WALL;
+        cellState = CellState.NONE;
         break;
       case CellType.START:
-        cellType = CellType.FLOOR
+        cellType = CellType.START;
+        cellState = CellState.OPEN;
         break;
       case CellType.END:
-        cellType = CellType.FLOOR
+        cellType = CellType.END;
+        cellState = CellState.OPEN;
         break;
     }
 
@@ -161,7 +196,7 @@ export class Game {
       row: row,
       col: col,
       type: type,
-      visited: false,
+      state: cellState!,
       parent: null,
       weight: null
     }
@@ -169,36 +204,45 @@ export class Game {
 
   // Retrieves the path, backwards, from end to finish, by getting the parent of the cells. The parents of cells are determined by the algorithm
   getPath(endCell: Cell) {
-    let currCell: Cell = endCell.parent!
+    let currCell: Cell = endCell
+    console.log('in getPath', currCell)
 
-    while (currCell.type !== CellType.START) {
+    while (1) {
       this.path.push(currCell)
+      if (currCell.parent === null)
+        break;
       currCell = currCell.parent!
     }
   }
 
-  loop(interval: number) {
-    this.timePassed += interval
+  loop = (timestamp: number) => {
+    const deltaTime: number = timestamp - this.lastTimestamp;
+    this.lastTimestamp = timestamp;
+    this.timePassed += deltaTime;
     if (this.state === GameState.RUNNING) {
       if (this.timePassed > this.stepTime) {
-        this.timePassed = 0
+        this.timePassed -= this.stepTime;
         const result: Cell | null = this.algorithm.step();
   
-        if (result === null)
-          return
-        else if (result.type === CellType.END) {
-          this.getPath(result)
-          this.renderer.renderPath(this.path)
-          this.state = GameState.FINISHED
+        if (result !== null) {
+          this.renderer.renderVisitedCell(result);
+          if (result.type === CellType.END) {
+            this.getPath(result);
+            this.renderer.renderPath(this.path);
+            this.state = GameState.FINISHED;
+            console.log('FINISHED')
+          }
         }
-        else
-          this.renderer.renderVisitedCell(result)
+        else {
+          this.state = GameState.FINISHED;
+          return;
+        }
       }
     }
     if (this.state !== GameState.RUNNING && this.state !== GameState.PAUSED)
-      return
+      return;
 
-    requestAnimationFrame(this.loop)
+    requestAnimationFrame(this.loop);
   }
 
 

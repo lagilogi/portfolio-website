102d977b5127a97dd5172a7f3b1cc599798b5299
lagilogi <wouter_s@hotmail.nl>
2026-05-05 18:45:19 +0200

    Improve cell styling in createCellDiv

--- pathfinding-algorithms-visualizer/Renderer.ts
+++ pathfinding-algorithms-visualizer/Renderer.ts
@@ -36,9 +36,11 @@ export class Renderer {
 			border: 1px solid var(--border);
 			font-size: 12px;
 			color: var(--bg-dark);
+			font-weight: 600;
 			text-align: center;
+			align-content: center;
 			vertical-align: middle;
-			line-height: ${this.sqrSize / 2}`
+			`
 		newCellDiv.setAttribute('id', cell.id)
 
     switch (cell.type) {
@@ -49,12 +51,14 @@ export class Renderer {
         newCellDiv.style.background = 'var(--bg-dark)'
         break;
       case CellType.START:
-        newCellDiv.style.background = 'lightblue'
-        newCellDiv.innerText = 'S'
+				newCellDiv.style.background = 'lightblue'
+				newCellDiv.style.border = '1px dotted var(--bg-light)'
+        newCellDiv.innerText = 'St'
         break;
       case CellType.END:
-        newCellDiv.style.background = 'lightgreen'
-        newCellDiv.innerText = 'E'
+				newCellDiv.style.background = 'lightgreen'
+				newCellDiv.style.border = '1px dotted var(--bg-light)'
+        newCellDiv.innerText = 'Ex'
         break;
     }
     return newCellDiv

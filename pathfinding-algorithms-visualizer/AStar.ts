98e8d7b1b3fc4de163cdf78fbb5270cbfb0e9935
lagilogi <wouter_s@hotmail.nl>
2026-05-15 18:20:38 +0200

    Add comments, work on diagonal calculation but is still broken

--- pathfinding-algorithms-visualizer/AStar.ts
+++ pathfinding-algorithms-visualizer/AStar.ts
@@ -1,5 +1,10 @@
 import { Cell, CellState, CellType, StepResult, Maze, PathfindingAlgorithm } from "./types";
 
+// - DIAGONAL_CELL_DISTANCE is based on moving from 1 to another diagonally, then use Pythagoras' theorem
+// to calculate the distance, which is ~1.4 * 10 = 14 for a nice integer value
+// - CELL_DISTANCE Value is based on moving from 1 cell to another, either horizontally or vertically. We multiply
+// that by 10 to keep the same value size as DIAGONAL_CELL_DISTANCE
+const DIAGONAL_CELL_DISTANCE = 14; 
 const CELL_DISTANCE = 10;
 
 export class AStar implements PathfindingAlgorithm {
@@ -27,18 +32,19 @@ export class AStar implements PathfindingAlgorithm {
 		return this.grid[row][col];
 	}
 
-	// Calculate the start, end and total costs for a neighbouring cell
-	calculateNewNeighbourCosts(currCell: Cell, neighbour: Cell) {
-		// Calculate the cost from the start cell to current cell. This value can change if there is a shorter way
-		neighbour.startCost = currCell.startCost + 10;
-
-		// Calculate the distance of neighbour cell to end cell according to Manhatten distance formula. This value will not change after being set the first time.
+	// Calculate the start, end and total costs for a neighbouring cell. If diagonal is enabled, then diagonal cells will be checked as well
+	// and given a different cell distance value, due to Pythagoras' theorem.
+	calculateNewNeighbourCosts(currCell: Cell, neighbour: Cell, distance: number) {
+		neighbour.startCost = currCell.startCost + distance;
 		neighbour.endCost = (Math.abs(neighbour.row - this.end.row) + Math.abs(neighbour.col - this.end.col)) * 10;
-
-		// Calculate total cost from start to neighbour cell, and from neighbour cell to end.
 		neighbour.totalCost = neighbour.startCost + neighbour.endCost
 	}
 
+	// This function adds all neighbouring cells that are not a wall and have CellState.OPEN to an array.
+	// - If the neighbouring cell is already queued and its startCost is higher than it could be, coming from the currCell,
+	// then that cell's startCost and parent gets updated.
+	// - If the neighbouring cell is already visited, then we check if the current cell's startCost could be lower, and if yes,
+	// we update currrent cell's startCost and parent.
 	addNeighboursToArray(currCell: Cell): Cell[] {
 		const newlyQueued: Cell[] = [];
 
@@ -61,27 +67,31 @@ export class AStar implements PathfindingAlgorithm {
 			]
 		
 		for (const [rowOffset, colOffset] of directions) {
-			const row = currCell.row + rowOffset;
-			const col = currCell.col + colOffset;
-			const neighbour = this.grid[row][col];
+			const row: number = currCell.row + rowOffset;
+			const col: number = currCell.col + colOffset;
+			const neighbour: Cell = this.grid[row][col];
+			let distance: number = CELL_DISTANCE;
+
+			if (this.diagonal)
+				distance = (row === 0 || col === 0) ? CELL_DISTANCE : DIAGONAL_CELL_DISTANCE
 
 			if (neighbour.type !== CellType.WALL) {
 				if (neighbour.state === CellState.OPEN) {
 					neighbour.state = CellState.QUEUED;
 					neighbour.parent = currCell;
-					this.calculateNewNeighbourCosts(currCell, neighbour);
+					this.calculateNewNeighbourCosts(currCell, neighbour, distance);
 	
 					this.array.push(neighbour);
 					newlyQueued.push(neighbour);
 				}
-				else if (neighbour.state === CellState.QUEUED && neighbour.startCost > currCell.startCost + CELL_DISTANCE) {
-					neighbour.startCost = currCell.startCost + CELL_DISTANCE;
+				else if (neighbour.state === CellState.QUEUED && neighbour.startCost > currCell.startCost + distance) {
+					neighbour.startCost = currCell.startCost + distance;
 					neighbour.parent = currCell;
 					newlyQueued.push(neighbour);
 				}
-				else if (neighbour.state === CellState.VISITED && currCell.startCost > neighbour.startCost + CELL_DISTANCE) {
-						currCell.startCost = neighbour.startCost + CELL_DISTANCE;
-						currCell.parent = neighbour;
+				else if (neighbour.state === CellState.VISITED && currCell.startCost > neighbour.startCost + distance) {
+					currCell.startCost = neighbour.startCost + distance;
+					currCell.parent = neighbour;
 				}
 			}
 		}
@@ -89,6 +99,8 @@ export class AStar implements PathfindingAlgorithm {
 		return newlyQueued;
 	}
 
+	// Find the cell with the lowest totalCost to queue as next cell to visit. If there are multiple cells with the same lowest
+	// value, then we check what cell is closest to the end and queue that one as next cell to visit.
 	findLowestCost(): Cell {
 		let lowestCostIndex: number = 0;
 		let nextCell: Cell[];
